5bfde645532c164e023d57553c8e06ee1cca8e1c
Richard Thompson <graham.thompson.613@gmail.com>
2021-11-28 11:13:03 -0500

    Implemented image sliders

--- src/components/carCard/index.jsx
+++ src/components/carCard/index.jsx
@@ -1,19 +1,37 @@
 import './styles.css';
 import {FaTachometerAlt, FaPalette} from 'react-icons/fa';
+import {Swiper, SwiperSlide} from "swiper/react";
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import SwiperCore, {Pagination} from 'swiper';
 
-
+SwiperCore.use([Pagination]);
 
 export const CarCard = (props) => {
     
     return(
         <div className="carCard">
             <div className="car-images">
-                <img src={props.images[0].stringValue} alt="" />
+                <Swiper
+                    slidesPerView={1}
+                    spaceBetween={25}
+                    centeredSlides={true}
+                    pagination={{"clickable": true}}
+                >
+                    {props.images.map( function (carImage) {
+                        return(
+                           <SwiperSlide>
+                                <img src={carImage.stringValue} alt="" />
+                            </SwiperSlide> 
+                        );
+                    })}
+                </Swiper>
+                
             </div>
             <div className="car-info-container">
                 <span className="make-model"><h3><strong>{props.year} {props.make}</strong> {props.model}</h3></span>
                 <div className="details">
-                    <p><strong><FaTachometerAlt/> {props.km} km</strong></p>
+                    <p><strong><FaTachometerAlt/> {props.km}km</strong></p>
                     <p><strong><FaPalette/> {props.color}</strong></p>
                 </div>
             </div>
